b210365b05ac826176df9e8263cbb64c05c6e8a6
Samuel Shen <sshen05@terpmail.umd.edu>
2023-10-30 15:50:51 -0400

    Finish validating zip code

--- form-validation/src/index.js
+++ form-validation/src/index.js
@@ -1,9 +1,14 @@
 import './style.css';
 import { listenFormControlInteraction } from './form-helper';
-import { EmailValidator } from './validators';
+import { EmailValidator, ZipCodeValidator } from './validators';
 
 listenFormControlInteraction();
 
 const emailInput = document.getElementById('email');
 const emailErrorOutput = document.getElementById('email-validation-output');
 new EmailValidator(emailInput, emailErrorOutput).init();
+
+const countrySelect = document.getElementById('country');
+const zipCodeInput = document.getElementById('zip-code');
+const zipCodeErrorOutput = document.getElementById('zip-code-validation-output');
+new ZipCodeValidator(countrySelect, zipCodeInput, zipCodeErrorOutput).init();
--- form-validation/src/validators.js
+++ form-validation/src/validators.js
@@ -1,26 +1,66 @@
+import postalCodes from 'postal-codes-js';
 
+/**
+ * Generic validator that only validates whether the input is required
+ */
 class Validator {
-    static validate(input) {
+    #input;
+    #errorOutput;
+
+    /**
+     * 
+     * @param {HTMLInputElement} input 
+     * @param {HTMLElement} errorOutput 
+     */
+    constructor(input, errorOutput) {
+        this.#input = input;
+        this.#errorOutput = errorOutput;
+    }
+
+    get input() {
+        return this.#input;
+    }
+
+    validate() {
         const errorMessages = [];
         let valid = true;
-        if (input.validity.valueMissing) {
+        if (this.#input.validity.valueMissing) {
             valid = false;
             errorMessages.push('This is a required field.');
         }
         return { valid, errorMessages };
     }
-    static reportValidation(validate, errorOutput) {
-        errorOutput.replaceChildren();
+
+    /**
+     * Provide error messages on errorOutput based on the result of the validate method.
+     * Take a validate callback to allow caller to overwrite the validate method
+     * @param {Function} validate 
+     */
+    #reportValidation(validate = this.validate) {
+        // Clear previous errors
+        this.#errorOutput.replaceChildren();
+        this.#input.setCustomValidity('');
+
         const validation = validate();
-        if(!validation.valid) {
+        if (!validation.valid) {
             const ul = document.createElement('ul');
-            for(const errorMessage of validation.errorMessages) {
+            for (const errorMessage of validation.errorMessages) {
                 const li = document.createElement('li');
                 li.textContent = errorMessage;
                 ul.append(li);
             }
-            errorOutput.append(ul);
+            this.#errorOutput.append(ul);
         }
+    }
+
+    init(validate) {
+        this.#input.addEventListener('input', (e) => {
+            this.#reportValidation(validate);
+        });
+        this.#input.addEventListener('blur', (e) => {
+            this.#reportValidation(validate);
+        });
+        // |Todo reportValidation on submit
     }
 }
 
@@ -31,8 +71,7 @@ Validator classes, construct with id, init function to attach event listeners
     - Return error message to add into span (maybe using the validityMessage)
     */
 class EmailValidator {
-    #input;
-    #errorOutput;
+    #validator
 
     /**
      * 
@@ -43,37 +82,65 @@ class EmailValidator {
         if (!(input instanceof HTMLInputElement) || (input.type !== 'email')) {
             throw new TypeError('Invalid input element, must be of type email');
         }
-        this.#input = input;
-        this.#errorOutput = errorOutput;
+        this.#validator = new Validator(input, errorOutput);
     }
 
     #validate() {
-        const generalValidation = Validator.validate(this.#input);
+        const generalValidation = this.#validator.validate();
         let { valid } = generalValidation;
         const { errorMessages } = generalValidation;
-        if(this.#input.validity.typeMismatch) {
+        if (this.#validator.input.validity.typeMismatch) {
             valid = false;
             errorMessages.push('Please enter a valid email address.');
         }
-        return {valid, errorMessages};
+        return { valid, errorMessages };
     }
 
     init() {
-        this.#input.addEventListener('input', (e) => {
-            Validator.reportValidation(this.#validate.bind(this), this.#errorOutput);
-        });
-        this.#input.addEventListener('blur', (e) => {
-            Validator.reportValidation(this.#validate.bind(this), this.#errorOutput);
-        });
+        this.#validator.init(this.#validate.bind(this));
     }
 }
 
 class ZipCodeValidator {
+    #validator;
+    #select;
 
+    /**
+     * @param {HTMLSelectElement} select Select element with value of country code in each option
+     * @param {HTMLInputElement} input 
+     * @param {HTMLElement} errorOutput 
+     */
+    constructor(select, input, errorOutput) {
+        if (!(input instanceof HTMLInputElement) || (input.type !== 'text')) {
+            throw new TypeError('Invalid input element, must be of type text');
+        }
+        this.#validator = new Validator(input, errorOutput);
+        this.#select = select;
+    }
+
+    #validate() {
+        const generalValidation = this.#validator.validate();
+        let { valid } = generalValidation;
+        const { errorMessages } = generalValidation;
+        const countryCode = this.#select.value;
+        const zipCode = this.#validator.input.value;
+        if(typeof postalCodes.validate(countryCode, zipCode) === 'string') {
+            valid = false;
+            const errorMessage = postalCodes.validate(countryCode, zipCode);
+            // Tells browser that there is an error so that the css styling works appropriately
+            this.#validator.input.setCustomValidity(errorMessage);
+            errorMessages.push(errorMessage);
+        }
+        return { valid, errorMessages };
+    }
+
+    init() {
+        this.#validator.init(this.#validate.bind(this));
+    }
 }
 
 class PasswordValidator {
 
 }
 
-export {EmailValidator};
+export { EmailValidator, ZipCodeValidator };
