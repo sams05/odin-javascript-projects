9e026edb6fab65cb0b00b687671912a678323911
Samuel Shen <sshen05@terpmail.umd.edu>
2023-12-29 13:16:26 -0500

    Finish implementing LinkedList

--- linked-lists/linked-list.js
+++ linked-lists/linked-list.js
@@ -10,30 +10,6 @@ class LinkedList {
         }
     }
 
-    prepend(value) {
-        // Works whether head is null or not
-        this.head = new LinkedList.$Node(value, this.head);
-        // If the LinkedList is originally empty, update the tail to point to the new node
-        if(this.tail === null) {
-            this.tail = this.head;
-        }
-        return this;
-    }
-
-    append(value) {
-        const newNode = new LinkedList.$Node(value, null);
-        // Case: empty list
-        if(this.head === null) {
-            this.head = newNode;
-            this.tail = newNode;
-            return this;
-        }
-        // Case: list is not empty
-        this.tail.nextNode = newNode;
-        this.tail = newNode;
-        return this;
-    }
-
     get size() {
         let count = 0;
         let cur = this.head;
@@ -44,6 +20,11 @@ class LinkedList {
         return count;
     }
 
+    /**
+     * Represents the LinkedList as strings. 
+     * The format should be: `( value ) -> ( value ) -> ( value ) -> null`
+     * @returns {String} the LinkedList represented as a string
+     */
     toString() {
         if(this.head === null) {
             return 'null';
@@ -59,12 +40,123 @@ class LinkedList {
 
         return str;
     }
+
+    /**
+     * Adds a new node containing value to the start of the list
+     * @param {*} value 
+     * @returns the updated list
+     */
+    prepend(value) {
+        // Works whether head is null or not
+        this.head = new LinkedList.$Node(value, this.head);
+        // If the LinkedList is originally empty, update the tail to point to the new node
+        if(this.tail === null) {
+            this.tail = this.head;
+        }
+        return this;
+    }
+
+    /**
+     * Adds a new node containing value to the end of the list
+     * @param {*} value 
+     * @returns the updated list
+     */
+    append(value) {
+        const newNode = new LinkedList.$Node(value, null);
+        // Case: empty list
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+        // Case: list is not empty
+        this.tail.nextNode = newNode;
+        this.tail = newNode;
+        return this;
+    }
+
+    /**
+     * Removes the last element from the list
+     * @returns Value of the removed element
+     */
+    pop() {
+        if(this.head === null) {
+            throw new Error('Array is empty');
+        }
+        // Single element in LinkedList
+        if(this.head === this.tail) {
+            const val = this.head.value;
+            this.head = null;
+            this.tail = null;
+            return val;
+        }
+
+        let cur = this.head;
+        // Traverse the list until we get to the second to last node
+        while(cur.nextNode !== null) {
+            if(cur.nextNode === this.tail) {
+                const val = cur.nextNode.value;
+                cur.nextNode = null;
+                this.tail = cur;
+                return val;
+            }
+            cur = cur.nextNode;
+        }
+    }
+
+    /**
+     * Returns the value at the given index
+     * @param {Number} index 
+     * @returns the value at the given index
+     */
+    at(index) {
+        // Invalid index
+        if(index === undefined || index < 0 || index >= this.size) {
+            return undefined;
+        }
+
+        let curNode = this.head;
+        let curIndex = 0;
+        while(this.curNode !== null) {
+            if(curIndex === index) {
+                return curNode.value;
+            }
+            curNode = curNode.nextNode;
+            curIndex++;
+        }
+    }
+
+    /**
+     * Returns true if the passed in value is in the list and otherwise returns false.
+     * @param {*} value 
+     * @returns {Boolean} whether the value is in the list
+     */
+    contains(value) {
+        let cur = this.head;
+        while(cur !== null) {
+            if(cur.value === value) {
+                return true;
+            }
+            cur = cur.nextNode;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the index of the node containing value, or null if not found.
+     * @param {*} value 
+     * @returns {Number|null} the index of the value
+     */
+    find(value) {
+        let curNode = this.head;
+        let curIndex = 0;
+        while(curNode !== null) {
+            if(curNode.value === value) {
+                return curIndex;
+            }
+            curNode = curNode.nextNode;
+            curIndex++;
+        }
+        return null;
+    }
 }
-
-const linkedList = new LinkedList();
-linkedList.append(1).append(2).append(1).append(31).append(192);
-console.log(linkedList.toString());
-
-const linkedList2 = new LinkedList();
-linkedList2.prepend(1).prepend(2).prepend(1).prepend(31).prepend(192);
-console.log(linkedList2.toString());
